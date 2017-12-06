4d427e77b56b4f5976f93682231ac9e559c4eaca
LBYPatrick <lbypatrick@gmail.com>
2017-12-05 20:01:02 -0500

    Update 11540's Auton + use AutonHelper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonFreshForward.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonFreshForward.java
@@ -6,20 +6,19 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.actuators.DriveTrain;
+import org.firstinspires.ftc.teamcode.actuators.*;
 import org.firstinspires.ftc.teamcode.databases.Statics;
 
 /**
  * Created by LBYPatrick on 11/14/2017.
  */
 @Autonomous(name = "FRESH_POS_FORWARD",group = "Freshman")
-public class AutonFreshForward extends LinearOpMode {
+final public class AutonFreshForward extends LinearOpMode {
 
     //Initialize objects
-    private     DriveTrain      mWheel;
-
-    private     Servo           GGrabberLObj;
-    private     Servo           GGrabberRObj;
+    private     DriveTrain      tankWheel;
+    private     AutonHelper     autonControl;
+    private     DcMotorControl  GIntake;
 
     // Declare OpMode members.
     final private ElapsedTime globalTime = new ElapsedTime();
@@ -27,14 +26,14 @@ public class AutonFreshForward extends LinearOpMode {
 
     private void initialize() {
 
-        DcMotor BL = hardwareMap.get(DcMotor.class, Statics.Freshman.LWheel);
-        DcMotor BR = hardwareMap.get(DcMotor.class, Statics.Freshman.RWheel);
+        DcMotor BL = hardwareMap.dcMotor.get(Statics.FRESH_L_WHEEL);
+        DcMotor BR = hardwareMap.dcMotor.get(Statics.FRESH_R_WHEEL);
+        DcMotor GIntakeMotor = hardwareMap.dcMotor.get(Statics.FRESH_INTAKE);
 
-        mWheel = new DriveTrain(BL,BR);
+        GIntake = new DcMotorControl(GIntakeMotor,false);
+        tankWheel = new DriveTrain(BL,BR);
 
-        //Glyph Grabbers
-        //GGrabberLObj = hardwareMap.get(Servo.class, Statics.Sophomore.Servos.left_glyphGrabber);
-        //GGrabberRObj = hardwareMap.get(Servo.class, Statics.Sophomore.Servos.right_glyphGrabber);
+        autonControl = new AutonHelper(this,tankWheel,false);
 
     }
 
@@ -51,15 +50,19 @@ public class AutonFreshForward extends LinearOpMode {
 
         initialize();
         waitForStart();
-        //Move Jewels
 
-        //Move Forward
-        stageTime.reset();
-        mWheel.tankDrive(-1,0);
-        if(!wait(0.328)) return;
-        mWheel.tankDrive(0,0);
+        //Take the glyph in
+        if(!autonControl.moveLiftUp(GIntake,2.0)) return;
+        if(!wait(1.5)) return;
 
+        //Move forward
+        if(!autonControl.moveForward(0.328)) return;
+        if(!wait(2.0)) return;
 
+        //Shoot the glyph out
+        if(!autonControl.moveLiftDown(GIntake,2.0)) return;
 
+        //Move a little back for not touching the glyph
+        if(!autonControl.moveBack(0.05)) return;
     }
 }
